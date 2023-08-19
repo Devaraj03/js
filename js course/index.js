dde06dbecf11a2ab2be046b1baa5cf6703c75f53
Devaraj03 <115549688+Devaraj03@users.noreply.github.com>
2023-08-19 12:47:46 +0530

    Add files via upload

---
+++ js course/index.js
@@ -0,0 +1,53 @@
+/*let object1=
+{
+    name1: "devaraj",
+    age:20,
+    gender:"male",
+    address:"2/22,abcd,xyz",
+    siblings:
+    {
+        brother:"hari",
+        sister:"harini"
+    }
+}//object
+object1.name1="deva";
+object1.age=33;
+//dot notation
+console.log(object1.name1);
+console.log(object1.gender);
+console.log(object1.address);
+//bracket notation
+console.log(object1['age']);
+//dot notation
+console.log(object1.siblings.brother);
+console.log(object1.siblings.sister);
+*/
+/*
+let favcolor=["red","white","black"]
+//-object{}
+//-array[]
+favcolor[3]="blue";
+console.log(favcolor)
+console.log(favcolor.length)
+console.log(favcolor.push(favcolor[4]="green"))
+console.log(favcolor.pop())
+console.log(favcolor)
+*/
+/*
+function greetuser(name)//set of statement or instruction
+{
+    let msg="hello i'm "+name+" and i'm using javascript";
+    console.log(msg)
+
+}
+function add(num1,num2)
+{
+    console.log("the added value is : "+num1+num2);
+}
+add(22,88.88)
+//greetuser("devz")
+*/
+x=10;
+x-=1
+console.log(x)
+console.log("hello world")
---
+++ js2/index2.js
@@ -0,0 +1,8 @@
+x=10;
+y=12;
+y--;
+x++;
+z=x+=1;
+console.log(x);
+console.log(y);
+console.log(z);
